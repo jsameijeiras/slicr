20967b43fe3d15cf39d87dcd7355c496decbf858
Jose <jsameijeiras@gmail.com>
2019-12-11 19:59:11 +0100

    POST file

--- slicr/views.py
+++ slicr/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import os
 
 def index (request):
-    hello = "Hello, world esto es un cambio"
-    return render(request, 'slicr/index.html', {'hello':hello})
+    if request.method == 'POST':
+
+        data = request.POST.copy()
+        firstname = data.get("firstname")
+        hello = "POST request"
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        filepath = os.path.join(settings.MEDIA_ROOT, filename)
+        return render(request, 'slicr/index.html', {'hello':hello, 'filepath': filepath, 'print_form': False})
+    hello = "GET request"
+    return render(request, 'slicr/index.html', {'hello':hello, 'print_form': True})
 # Create your views here.
