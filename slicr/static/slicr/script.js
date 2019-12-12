a6a6632b0e79db57068055c25a6bb74d1158dd7b
Nil <nil@seetransparent.com>
2019-12-12 09:10:31 +0000

    Add loading button

--- slicr/static/slicr/script.js
+++ slicr/static/slicr/script.js
@@ -4,6 +4,12 @@ if (fileInput) {
     if (fileInput.files.length > 0) {
       const fileName = document.querySelector('#file-js-example .file-name');
       fileName.textContent = fileInput.files[0].name;
+      
+      const button = document.querySelector('#submit-button');
+      button.disabled = false;
+      button.onclick = () => {
+        button.className += ' is-loading';
+      };
     }
   }
 }
