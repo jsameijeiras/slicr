de29b38df9a02b56cab8f7b5c6f77747f748a2f2
Nil <nil@seetransparent.com>
2019-12-12 08:45:04 +0000

    Fix js

--- slicr/static/slicr/script.js
+++ slicr/static/slicr/script.js
@@ -1,7 +1,9 @@
 const fileInput = document.querySelector('#file-js-example input[type=file]');
-fileInput.onchange = () => {
-  if (fileInput.files.length > 0) {
-    const fileName = document.querySelector('#file-js-example .file-name');
-    fileName.textContent = fileInput.files[0].name;
+if (fileInput && fileInput.onchange) {
+  fileInput.onchange = () => {
+    if (fileInput.files.length > 0) {
+      const fileName = document.querySelector('#file-js-example .file-name');
+      fileName.textContent = fileInput.files[0].name;
+    }
   }
 }
