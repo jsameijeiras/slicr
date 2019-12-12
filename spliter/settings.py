5bbf7c58d20fbe712167a4a41f5e2c2bdbb6c8bd
Francisco Javier Barón Izquierdo <baronizquierdo@gmail.com>
2019-12-12 09:02:44 +0100

    Added all hosts

--- spliter/settings.py
+++ spliter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_brkus1z_om7*psh6t(ot1y_jf6qfn0=i6_ttdk!d+00^c#)+z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
